768b5044f3f73bf96487c9211686f96c47a6320d
BarrosNio <aldierisbrito@gmail.com>
2026-04-02 16:08:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -79,5 +79,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Socket.io WebRTC Server running on http://localhost:${PORT}`);
+  console.log("Server running on port " + PORT);
 });
